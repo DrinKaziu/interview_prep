0dc4eabd92a34bdaa9d89eca0f8f87201c364d09
SnowSurfer <drinkaziu@gmail.com>
2018-09-07 14:40:05 -0400

    countUniqueValues 2

--- multiple_pointers/countUniqueValues.js
+++ multiple_pointers/countUniqueValues.js
@@ -1,4 +1,4 @@
-function countUniqueValues(arr){
+function countUniqueValues(arr) {
   let uniqueValues = [];
   for (let i = 0; i < arr.length; i++) {
     let pointer1 = arr[i];
@@ -14,3 +14,18 @@ function countUniqueValues(arr){
 }
 
 countUniqueValues([1, 3, 3, 2, 2, 2, 6, 100, 100]);
+
+// Solution #2 (better)
+function countUniqueValues(arr){
+  if(arr.length === 0){
+    return 0;
+  }
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if(arr[i] !== arr[j]) {
+      i++
+      arr[i] = arr[j]
+    }
+  }
+  return i + 1;
+}
